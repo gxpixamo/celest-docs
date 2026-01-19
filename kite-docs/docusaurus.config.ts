7bb67b109c8b5e081abe6425e6a12b359bf7bc9f
Gx Pixamo <ranishema63@gmail.com>
2026-01-20 00:39:13 +0600

    Update site URL and navbar title in config

--- kite-docs/docusaurus.config.ts
+++ kite-docs/docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://docs.kite.onl",
+  url: "https://docs.celestnetwork.qzz.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -62,7 +62,7 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/logo.png",
     navbar: {
-      title: "Kite",
+      title: "Celest Netwoek",
       logo: {
         alt: "Kite Logo",
         src: "img/logo.svg",
